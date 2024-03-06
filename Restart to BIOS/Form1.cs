5edf1550c28ac351c60c795e1c70b49020f61a64
awolf <awolf@SERVER-PC>
2024-03-05 20:43:54 -0500

    Add project files.

---
+++ Restart to BIOS/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Restart_to_BIOS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown.exe", "-r -fw -t 0");
+        }
+    }
+}
